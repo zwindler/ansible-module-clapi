44fddaa27f13c5d4e8ed5c5e0bfd17c218012e24
GERMAIN Denis - ISOCEL <denis.germain@isocel.info>
2016-08-08 18:10:03 +0200

    New module to add/remoev hostgroups Plan is to add also addmember delmember capability soon

---
+++ library/clapi_hostgroup.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+#This file is part of ansible_module_clapi
+#Copyright (C) 2016  Denis GERMAIN
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+DOCUMENTATION = '''
+---
+module: clapi_hostgroup
+short_description: Centreon CLAPI hostgroup creates/updates/deletes
+description:
+   - This module allows you to create, modify and delete Centreon hostgroups 
+   - More informations on CLAPI can be found on 
+   - https://documentation.centreon.com/docs/centreon-clapi/en/latest/
+version_added: "0.1"
+author:
+    - "Denis GERMAIN (@zwindler)"
+requirements:
+    - "python >= 2.6"
+    - Centreon with CLAPI
+options:
+    "username": {"required": True, "type": "str"},
+    "password": {"required": True, "type": "str"},
+    "hostgroupname": {"required": True, "type": "str"},
+    "hostgroupalias": {"required": False, "type": "str"},
+    "state": {
+        "default": "present", 
+            "choices": ['present', 'absent'],  
+            "type": 'str' 
+        },
+
+    username:
+        description:
+            - Centreon username (must be admin to be allowed to use CLAPI)
+        required: true
+    password:
+        description:
+            - Centreon user password
+        required: true
+    hostgroupname:
+        description:
+            - hostgroupname to remove/add to centreon configuration
+        required: true
+    pollername:
+        description:
+            - Select which Centreon host (poller) is targeted. By design, first 
+            - Centreon poller is called Central
+        required: false
+        default: "Central"
+    state:
+        description:
+            - Choose whether the hostgroup must be present or absent from Centreon
+            - poller configuration file
+            - C(absent) removes hostgroup from configuration if needed
+            - C(present) adds host from configuration if needed
+        required: true
+        choices: ['absent', 'present']
+        default: "present"
+'''
+
+EXAMPLES = '''
+#Add a test hostgroup, delegated to centreon poller
+- hosts: localhost
+  tasks:
+    - name: add test hostgroup
+      clapi_hostgroup:
+        username: clapi
+        password: clapi
+        hostgroupname: testgroup
+        state: present
+      delegate_to: ces
+'''
+
+def base_command(username, password):
+    #TODO find centreon path
+    return "centreon -u "+username+" -p "+password+" -o HG "
+
+def run_command(fullcmd):
+    proc = subprocess.Popen(shlex.split(fullcmd), stdout=subprocess.PIPE)
+    return proc.communicate()[0],proc.returncode
+
+def hostgroup_absent(data):
+    #building command
+    basecmd = base_command(data['username'], data['password'])
+    operation = "-a del "
+    varg = '-v "'+data['hostgroupname']+'"'
+    #running full command
+    (cmdout, rc) = run_command(basecmd+operation+varg)
+
+    if rc == 0:
+        has_changed = True
+        meta = {"absent": "successfully removed"}
+        return (has_changed, meta)
+    else:
+        if cmdout.find("Object not found") == 0:
+            has_changed = False
+            meta = {"absent": cmdout}
+            return (has_changed, meta)
+        else:
+            print json.dumps({
+                "failed" : True,
+                "msg"    : "centreon command failed with error: "+cmdout
+            })
+            sys.exit(1)
+
+def hostgroup_present(data):
+    #building command
+    basecmd = base_command(data['username'], data['password'])
+    operation = "-a add "
+    varg = '-v "'+data['hostgroupname']+';'
+    if data['hostgroupalias']:
+        varg += data['hostgroupalias']+';"'
+    else:
+        varg += data['hostgroupname']+';"'
+    #running full command
+    (cmdout, rc) = run_command(basecmd+operation+varg)
+
+    if rc == 0:
+        has_changed = True
+        meta = {"present": "successfully added"}
+        return (has_changed, meta)
+    else:
+        if cmdout.find("Object already exists") == 0:
+            has_changed = False
+            meta = {"present": cmdout}
+            return (has_changed, meta)
+        else:
+            print json.dumps({
+                "failed" : True,
+                "msg"    : "centreon command failed with error: "+cmdout
+            })
+            sys.exit(1)
+
+def main():
+
+    fields = {
+        "username": {"required": True, "type": "str"},
+        "password": {"required": True, "type": "str"},
+        "hostgroupname": {"required": True, "type": "str"},
+        "hostgroupalias": {"required": False, "type": "str"},
+        "state": {
+            "default": "present", 
+            "choices": ['present', 'absent'],  
+            "type": 'str' 
+        },
+    }
+    choice_map = {
+      "present": hostgroup_present,
+      "absent": hostgroup_absent, 
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    has_changed, result = choice_map.get(module.params['state'])(module.params)
+    module.exit_json(changed=has_changed, meta=result)
+
+from ansible.module_utils.basic import *
+import shlex, subprocess, sys
+
+if __name__ == '__main__':  
+    main()
